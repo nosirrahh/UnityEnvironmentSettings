1e8c6108b291ba583468d9ab956b755be33ae3aa
nosirrahh <nosirrahh@gmail.com>
2025-01-02 16:24:33 -0300

    Atualizado BuildSettingsScriptableObject.cs

--- Assets/UnityEnvironmentSettings/Editor/BuildSettingsScriptableObject.cs
+++ Assets/UnityEnvironmentSettings/Editor/BuildSettingsScriptableObject.cs
@@ -6,7 +6,7 @@ namespace Nosirrahh.UnityEnvironmentSettings.Editor
     /// ScriptableObject class that stores build settings and environment provider.
     /// Uses the creation menu to generate an instance of this object in the project.
     /// </summary>
-    [CreateAssetMenu (fileName = "BuildSettings", menuName = "Nosirrahh/UnityEnvironmentSettings/BuildSettings", order = 1)]
+    [CreateAssetMenu (fileName = "BuildSettings", menuName = "Packages/UnityEnvironmentSettings/BuildSettings", order = 1)]
     public class BuildSettingsScriptableObject : ScriptableObject
     {
         #region Fields
